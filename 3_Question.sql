2ecc4cf18cbf5d2e9b1acab329dd27a9681d0f3d
Ali Yılmaz <123991935+aliylmztr@users.noreply.github.com>
2023-05-24 23:51:28 +0300

    Update 3_Question.sql

--- 3_Question.sql
+++ 3_Question.sql
@@ -1,4 +1,4 @@
---customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
+--customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayıları nelerdir?
 
 SELECT store_id, COUNT(*) FROM customer
 GROUP BY store_id;
